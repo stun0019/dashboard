6d4a1feecfbcdfd3f54dad49aecef650895a7dd7
overdose <ootdacc519@gmail.com>
2026-09-03 10:16:25 +0800

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,1 +1,33 @@
+export const APP_CONFIG = {
 
+    defaultExchange: "OKX",
+
+    market: {
+
+        base: "BTC",
+
+        quote: "USDT"
+
+    },
+
+    okx: {
+
+        wsUrl:
+            "wss://ws.okx.com:8443/ws/v5/public",
+
+        instrument:
+            "BTC-USDT-SWAP"
+
+    },
+
+    bingx: {
+
+        wsUrl:
+            "wss://open-api-swap.bingx.com/swap-market",
+
+        symbol:
+            "BTC-USDT"
+
+    }
+
+};
